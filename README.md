710d990d1eefb1b5862578df3bf04c8922501d1c
henur <henrycastordossantos@gmail.com>
2020-07-17 22:01:47 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-dccr42
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-dccr42)
---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Coinprice';
+
+fieldArray: Array<any> = [];
+  newAttribute: any = {};
+
+  firstField = true;
+  firstFieldName = 'First Item name';
+  isEditItems: boolean;
+
+  addFieldValue(index) {
+    this.fieldArray.push(this.newAttribute);
+    this.newAttribute = {};
+  }
+
+  deleteFieldValue(index) {
+    this.fieldArray.splice(index, 1);
+  }
+
+  onEditCloseItems() {
+    this.isEditItems = !this.isEditItems;
+  }
+}
